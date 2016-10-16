bf7df54f6bd8078dd16731e40c5c0c75071babd5
Luandro <luandro@gmail.com>
2016-10-16 11:55:35 -0200

    styled and added imgs to Menu footer

--- app/components/MenuFooter/index.js
+++ app/components/MenuFooter/index.js
@@ -8,6 +8,7 @@ import React from 'react';
 
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import UfvLogo from '../LayoutWrapper/ufv.png';
 
 import styles from './styles.css';
 
@@ -15,6 +16,11 @@ function MenuFooter() {
   return (
     <footer className={styles.wrapper}>
       <FormattedMessage {...messages.header} />
+      <div className={styles.imgs}>
+        <img src={UfvLogo} alt="Universidade Federal de Viçosa" />
+        <img src={UfvLogo} alt="Universidade Federal de Viçosa" />
+        <img src={UfvLogo} alt="Universidade Federal de Viçosa" />
+      </div>
       <h4>por Luandro</h4>
     </footer>
   );
